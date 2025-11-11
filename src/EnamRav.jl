8511cde4c2c540f9649f0a84741f96a2c63e2d7e
Penelope Yong <penelopeysm@gmail.com>
2025-11-11 01:58:04 +0000

    negative example

--- src/EnamRav.jl
+++ src/EnamRav.jl
@@ -17,18 +17,17 @@ struct Indx{inds,T<:Lens} <: Lens end  # inds is a tuple of indices
 
 struct VN{sym,L<:Lens} end
 macro vn(expr)
-    _vn(expr, Iden)
+    esc(_vn(expr, :Iden))
 end
-function _vn(sym::Symbol, inner)
-    return VN{sym,inner}()
+function _vn(sym::Symbol, inner_expr)
+    return :($VN{$(QuoteNode(sym)),$inner_expr}())
 end
-function _vn(expr::Expr, inner)
+function _vn(expr::Expr, inner_expr)
     next_inner = if expr.head == :(.)
-        # expr.args[2] isa QuoteNode, so we get the symbol with `value` 
-        Prop{expr.args[2].value,inner}
+        :(Prop{$(expr.args[2]),$inner_expr})
     elseif expr.head == :ref
         # todo (or to-never-do): can't handle non-integer indices
-        Indx{tuple(expr.args[2:end]...),inner}
+        :(Indx{tuple($(expr.args[2:end]...)),$inner_expr})
     else
         error("you will pay for this when the robots take over the world")
     end
@@ -92,6 +91,65 @@ function demo()
         println()
     end
 end
+
+"""
+# The above design allows for completely type stable indexing
+# into VNM, BUT this only holds true if the VN type is known.
+# If the VN type depends on a variable (e.g. an index in a loop),
+# then this will struggle. Here's a demo:
+
+using EnamRav
+N = 50
+function make_dict(N)
+    d = Dict{VN,Float64}()
+    for i in 1:N
+        d[@vn(x[i])] = 1.0
+    end
+    return d
+end
+# First time takes AGES! probably to do with the
+# fact that every x[i] is a different type...
+@time make_dict(N);
+@time make_dict(N);
+
+d = make_dict(N)
+vnm = VNM(d)
+function f(vnm)
+    local sum = 0.0
+    for i in 1:N
+        sum += vnm[@vn(x[i])]
+    end
+    return sum
+end
+@time f(vnm)  # 99 ms first time
+@time f(vnm)  # 88 µs subsequent times
+# Here you can observe one of the problems of this data type:
+# because `i` is part of the type, it cannot infer the type
+# of `@vn(x[i])` inside the loop, and consequently it cannot
+# infer the type of `vnm[@vn(x[i])]`, even though they are
+# all Float64s.
+@code_typed f(vnm)
+
+# This is what it should be:
+xs = fill(1.0, N)
+@time sum(xs)  # 13 ms first time
+@time sum(xs)  # 4 µs, 1 alloc
+
+# Of course, the way to get around it is to do everything
+# at the type level by unrolling the loop...
+@generated function f2(vnm)
+    expr = quote
+        sum = 0.0
+    end
+    for i in 1:N
+        push!(expr.args, :(vnm[@vn(x[i])]))
+    end
+    push!(expr.args, :(return sum))
+end
+@time f2(vnm); # 10 ms
+@time f2(vnm); # 3 µs, 0 allocs
+
+"""
 
 
 end # module EnamRav
